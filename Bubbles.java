98fb057ab910e8d70e18bf0fe3bc0d7e122d4c3d
juliannanichols <junichols17@saintjosephhs.com>
2016-05-23 20:59:43 -0400

    updating to where we were in class..

--- Bubbles.java
+++ Bubbles.java
@@ -1,4 +1,43 @@
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
 
-public class Bubbles {
+/**
+ * 
+ * @author Julianna Nichols
+ * @author Isabelle Schroeder
+ */
+public class Bubbles extends JFrame implements ActionListener, Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	public Bubbles() {
+		super();
+
+		JMenuBar menuBar;
+		JMenu menu;
+		JMenuItem menuItem;
+		JMenu subMenu;
+		
+		menuBar = new JMenuBar();
+
+		menu = new JMenu(""); // title
+		menuItem = new JMenuItem(""); // menu item
+		menuItem.setActionCommand("");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+		
+		menuBar.add(menu);
+		
+		this.setSize(600, 600);
+		this.setVisible(true);
+	}
+	
+	/**
+	 * 
+	 */
+	public void actionPerformed(ActionEvent e) {
+		
+	}
 
 }
--- Jelly.java
+++ Jelly.java
@@ -1,4 +1,17 @@
+import javax.swing.*;
 
+/**
+ * 
+ * @author Julianna Nichols
+ * @author Isabelle Schroeder
+ */
 public class Jelly {
-
+	
+	/**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bubbles myFrame = new Bubbles();
+        myFrame.setExtendedState(JFrame.MAXIMIZED_BOTH); //so it fills whole screen
+    }
 }
