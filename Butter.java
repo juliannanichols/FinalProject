b604d90e7337f88a0d6383b02d39f8fe6fe628d6
isschroeder <kittypuppybird@gmail.com>
2016-06-02 12:43:26 -0400

    Actually, did some work

--- Butter.java
+++ Butter.java
@@ -1,4 +1,3 @@
-import java.awt.event.*;
 import java.util.*; //needed to use Random
 import javax.swing.*;
 import com.sun.prism.*; //needed to use Image
@@ -10,21 +9,21 @@ import com.sun.prism.*; //needed to use Image
  * @author Julianna Nichols
  * @author Isabelle Schroeder
  */
-public class Butter implements ActionListener {
-	
-	//Bubbles bub = new Bubbles();
+public class Butter {
 	
 	/**
 	 * This method puts all the pictures into each
 	 * corresponding array
 	 */
-	public void makeArray(String name, int numCards) {
+	public ImageIcon[] makeArray(String name, int numCards) {
 		ImageIcon[] pictures = new ImageIcon[numCards];
 		
 		switch(name){
 		case "Animals":
 			for (int i = 0; i < numCards/2; i++ ) {
-				int rnd = new Random().nextInt(pictures.length);
+				// may work, maybe not. The 9 is there so that the
+				// array will pick from 0-9 hopefully. Thanks stackOverflow
+				int rnd = new Random().nextInt(9);
 				pictures[i] = new ImageIcon("a" + rnd + ".png");
 	        }
 			
@@ -33,45 +32,23 @@ public class Butter implements ActionListener {
 				//call random on copy of array list
 				
 				// random for list of cards and for buttons
-				
 			break;
 		case "Plants":
 			for (int i = 0; i < numCards/2; i++ ) {
-				int rnd = new Random().nextInt(pictures.length);
-				pictures[i] = new ImageIcon("p" + i + ".png");
+				int rnd = new Random().nextInt(9);
+				pictures[i] = new ImageIcon("p" + rnd + ".png");
 	        }
 			
 			break;
 		
 		case "Traditional":
 			for (int i = 0; i < numCards/2; i++ ) {
-				int rnd = new Random().nextInt(pictures.length);
-				pictures[i] = new ImageIcon("t" + i + ".png");
+				int rnd = new Random().nextInt(9);
+				pictures[i] = new ImageIcon("t" + rnd + ".png");
 			}
 			break;
 		}
-	
+		return pictures;
 	}
-	
-	/**
-	 * 
-	 * @param name
-	 */
-	public void setButtons(){
-		
-	}
-	
-	/**
-	 * 
-	 */
-	public void setBack(){
-	
-	}
-	
-	/**
-	 * 
-	 */
-	public void actionPerformed(ActionEvent e) {
-		// possibly do while loop?	
-	}
+
 }
