466e625f53997791df7f049c0b4f18f0867a54ee
isschroeder <kittypuppybird@gmail.com>
2016-06-05 16:08:05 -0400

    Yay Butter!
    It probably maybe works now? But Bubbles is still very sad, so maybe
    not?

--- Butter.java
+++ Butter.java
@@ -20,53 +20,60 @@ public class Butter extends ArrayDeque<ImageIcon>{
 	public ArrayDeque<ImageIcon> makeArray(String name, int numCards) {
 		
 		ArrayDeque<ImageIcon> pictures = new ArrayDeque<ImageIcon>(numCards);
-		ArrayDeque<ImageIcon> otherPictures = new ArrayDeque<ImageIcon>(10);
-		int i = 0;
+		ArrayDeque<ImageIcon> otherPictures = pictures.clone();
 		
 		switch(name){
 		case "Animals":
-			do{ 
-				otherPictures.add( new ImageIcon( "a" + i + ".jpg"));
-				i++;
-			} while( i <= 10 );
-			
-			for( i = 0; i < numCards/2; i++){
+			for( int i = 0; i < numCards/2; i++){
 				// May work, maybe not. The 9 is there so that the array will pick from 0-9 hopefully. 
 				// Thanks stackOverflow :D
 				int rnd = new Random().nextInt(9);
 				
+				otherPictures.add( new ImageIcon( "a" + rnd + ".jpg" ) );
 				
-				//make array with all of the pictures in, set new array to have numCards length
-				//get random picture from original array and set to new array
-				//delete from original array
-				
-				pictures.add( new ImageIcon("a" + rnd + ".jpg")); // adds image to the end of the deque
-				otherPictures.remove(new ImageIcon("a" + rnd + ".jpg")); // should remove the image from the deque
+				// Should prevent repeat pictures!
+				if( pictures.contains( new ImageIcon( "a" + rnd + ".jpg" )) ){
+					i--; // so there will still be the correct number of cards
+				} else {
+					// Adds image to the end of the deque AND removes it from otherPictures!! 
+					// Use both getFirst and poll so that there will be 2 of each card!
+					// How spiffy!!!! :)
+					pictures.add( otherPictures.getFirst() );
+					pictures.add( otherPictures.poll() );
+				}
 	        }
-			
-				//dq
-				// remove images from the list
-				//call random on copy of array list
-				
-				// random for list of cards and for buttons
 			break;
-		case "Plants":
-			for( i = 0; i < numCards/2; i++ ) {
+			
+		case "Plants": // see Animals for cool little commenty things
+			for( int i = 0; i < numCards/2; i++){
 				int rnd = new Random().nextInt(9);
-				pictures.add( new ImageIcon("p" + rnd + ".jpg"));
-				otherPictures.remove(new ImageIcon("p" + rnd + ".jpg"));
+				otherPictures.add( new ImageIcon( "p" + rnd + ".jpg" ) );
+				
+				if( pictures.contains( new ImageIcon( "p" + rnd + ".jpg" )) ){
+					i--;
+				} else {
+					pictures.add( otherPictures.getFirst() );
+					pictures.add( otherPictures.poll() );
+				}
 	        }
 			break;
 		
 		case "Traditional":
-			for( i = 0; i < numCards/2; i++ ) {
+			for( int i = 0; i < numCards/2; i++){
 				int rnd = new Random().nextInt(9);
-				pictures.add( new ImageIcon("t" + rnd + ".png"));
-				otherPictures.remove(new ImageIcon("t" + rnd + ".png"));
+				// the traditional pictures are png files, while the plant and animal are jpg files
+				otherPictures.add( new ImageIcon( "t" + rnd + ".png" ) );
+				
+				if( pictures.contains( new ImageIcon( "t" + rnd + ".png" )) ){
+					i--;
+				} else {
+					pictures.add( otherPictures.getFirst() );
+					pictures.add( otherPictures.poll() );
+				}
 			}
 			break;
 		}
-		return pictures;
+		return pictures; // Yay! ^^
 	}
 
 }
