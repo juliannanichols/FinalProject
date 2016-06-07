67ac9c5cb3fee5b448fd13ee9e4460c3fd3a2706
isschroeder <kittypuppybird@gmail.com>
2016-06-07 01:36:39 -0400

    I'm pretty sure this is just uncommented stuff

--- Jelly.java
+++ Jelly.java
@@ -13,20 +13,20 @@ public class Jelly {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//    	String input = null; // so the user can have options
-//    	Butter but = new Butter();
-//    	int numCards = 6; // 6 is default
-//    	Bubbles bubble = new Bubbles();
-//    	String [] switchDeck = { "Animals" , "Plants" , "Traditional" }; // deck options
-//    	ArrayDeque<ImageIcon> pictures = new ArrayDeque<ImageIcon>(numCards); // deque of images!
+    	String input = null; // so the user can have options
+    	Butter but = new Butter();
+       	int numCards = 6; // 6 is default
+    	Bubbles bubble = new Bubbles();
+    	String [] switchDeck = { "Animals" , "Plants" , "Traditional" }; // deck options
+    	ArrayDeque<ImageIcon> pictures = new ArrayDeque<ImageIcon>(numCards); // deque of images!
     	
         Bubbles myFrame = new Bubbles();
         myFrame.setExtendedState(JFrame.MAXIMIZED_BOTH); //so it fills whole screen
-//        input = (String) JOptionPane.showInputDialog(null, "Pick which deck you would like to play with..",
-//				"The Choice of a Lifetime", JOptionPane.QUESTION_MESSAGE, null, switchDeck, switchDeck[0]);
-//		pictures = but.makeArray(input, numCards);
-//		// this is what's actually causing the NullPointer to be thrown, but I don't know why...
-//		// I checked, and pictures is not empty, so setButtons should be receiving the arrayDeque
-//		bubble.setButtons(pictures);
+        input = (String) JOptionPane.showInputDialog(null, "Pick which deck you would like to play with..",
+				"The Choice of a Lifetime", JOptionPane.QUESTION_MESSAGE, null, switchDeck, switchDeck[0]);
+		pictures = but.makeArray(input, numCards);
+		// this is what's actually causing the NullPointer to be thrown, but I don't know why...
+		// I checked, and pictures is not empty, so setButtons should be receiving the arrayDeque
+		bubble.setButtons(pictures);
     }
 }
